50c09071c8e5dde2ce90b90c5044c65a100f4139
sergvoronich <85866901+sergvoronich@users.noreply.github.com>
2022-01-04 10:36:50 +0300

    initial upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ClassCounter from "./components/ClassCounter";
+import Counter from "./components/Counter";
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+import "./styles/App.css";
 
 function App() {
+
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'JavaScript 1', body: 'Description' },
+    { id: 2, title: 'JavaScript 2', body: 'Description' },
+    { id: 3, title: 'JavaScript 3', body: 'Description' },
+  ])
+
+  const [posts2, setPosts2] = useState([
+    { id: 1, title: 'Python 1', body: 'Description' },
+    { id: 2, title: 'Python 2', body: 'Description' },
+    { id: 3, title: 'Python 3', body: 'Description' },
+  ])
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="App" >
+      <PostList posts={posts} title="Список постов 1" />
+      <PostList posts={posts2} title="Список постов 2" />
+    </div >
+  )
 }
 
 export default App;
