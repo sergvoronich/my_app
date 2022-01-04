936cf50e664498a05670598c4820c683d4da4fc8
Serg <sergvoronich@mail.ru>
2022-01-04 18:56:18 +0300

    further update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
+import { useRef } from "react";
 import ClassCounter from "./components/ClassCounter";
 import Counter from "./components/Counter";
+import PostForm from "./components/PostForm";
 import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 import "./styles/App.css";
 
 function App() {
@@ -13,18 +17,14 @@ function App() {
     { id: 3, title: 'JavaScript 3', body: 'Description' },
   ])
 
-  const [posts2, setPosts2] = useState([
-    { id: 1, title: 'Python 1', body: 'Description' },
-    { id: 2, title: 'Python 2', body: 'Description' },
-    { id: 3, title: 'Python 3', body: 'Description' },
-  ])
-
-
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost])
+  }
 
   return (
     <div className="App" >
+      <PostForm create={createPost} />
       <PostList posts={posts} title="Список постов 1" />
-      <PostList posts={posts2} title="Список постов 2" />
     </div >
   )
 }
