28dd62a6bf372442bd4a4d318b9dd13da837cb25
sergvoronich <85866901+sergvoronich@users.noreply.github.com>
2022-01-07 10:31:24 +0300

    further update

--- src/App.js
+++ src/App.js
@@ -1,73 +1,72 @@
-import React, { useMemo, useState } from "react";
+import axios from "axios";
+import React, { useEffect, useMemo, useState } from "react";
 import { useRef } from "react";
+import PostService from "./API/PostService";
 import ClassCounter from "./components/ClassCounter";
 import Counter from "./components/Counter";
+import PostFilter from "./components/PostFilter";
 import PostForm from "./components/PostForm";
 import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
+import Loader from "./components/UI/Loader/Loader";
+import MyModal from "./components/UI/MyModal/MyModal";
 import MySelect from "./components/UI/select/MySelect";
+import { usePosts } from "./hooks/usePosts";
 import "./styles/App.css";
 
 function App() {
 
-  const [posts, setPosts] = useState([
-    { id: 1, title: 'aaa', body: 'ggggggg' },
-    { id: 2, title: 'sss', body: 'kkkkkkk' },
-    { id: 3, title: 'bbb', body: 'aaaaaaa' },
-  ])
+  const [posts, setPosts] = useState([])
 
-  const [selectedSort, setSelectedSort] = useState('')
-  const [searchQuery, setSearchQuery] = useState('')
-
-  const sortedPosts = useMemo(() => {
-    if (selectedSort) {
-      return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]))
-    }
-    return posts
-  }, [selectedSort, posts])
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter(post => post.title.toLowerCase().includes(searchQuery))
-  }, [searchQuery, sortedPosts])
+  const [filter, setFilter] = useState({ sort: '', query: '' })
+  const [modal, setModal] = useState(false)
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+  const [isPostLoading, setIsPostLoading] = useState(false)
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
+    setModal(false)
   }
+
+  async function fetchPosts() {
+    setIsPostLoading(true)
+    const posts = await PostService.getAll()
+    setPosts(posts)
+    setIsPostLoading(false)
+  }
+
+  useEffect(() => {
+    fetchPosts()
+  }, [filter])
 
   const removePost = (post) => {
     setPosts(posts.filter(p => p.id !== post.id))
   }
 
-  const sortPosts = sort => {
-    setSelectedSort(sort)
-  }
 
   return (
     <div className="App" >
-      <PostForm create={createPost} />
+      <MyButton
+        style={{ marginTop: "30px" }}
+        onClick={() => setModal(true)}
+      >Создать пользователя</MyButton>
+      <MyModal
+        visible={modal}
+        setVisible={setModal}
+      >
+        <PostForm create={createPost} />
+      </MyModal>
       <hr style={{ margin: '15px 0' }} />
-      <div>
-        <MyInput
-          placeholder='Поиск...'
-          value={searchQuery}
-          onChange={e => setSearchQuery(e.target.value)}
-        />
-        <MySelect
-          value={selectedSort}
-          onChange={sortPosts}
-          defaultValue='Сортировка по'
-          options={[
-            { value: "title", name: "По названию" },
-            { value: "body", name: "По описанию" }]}
-        />
-      </div>
-      {sortedAndSearchedPosts.length !== 0
-        ? <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Список постов 1" />
-        : <h1 style={{ textAlign: 'center' }}>Посты не найдены!</h1>
+      <PostFilter
+        filter={filter}
+        setFilter={setFilter}
+      />
+      {isPostLoading
+        ? <div className='loader-container'><Loader /></div>
+        : <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Список постов 1" />
       }
-
     </div >
   )
 }
---
+++ src/components/UI/MyModal/MyModal.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import st from './MyModal.module.css'
+
+const MyModal = ({ children, visible, setVisible }) => {
+
+    const rootClasses = [st.myModal]
+    if (visible) rootClasses.push(st.active)
+
+    return (
+        <div
+            className={rootClasses.join(' ')}
+            onClick={() => setVisible(false)}
+        >
+            <div
+                className={st.myModalContent}
+                onClick={(e) => e.stopPropagation()}
+            >
+                {children}
+            </div >
+        </div >
+    );
+};
+
+export default MyModal;
